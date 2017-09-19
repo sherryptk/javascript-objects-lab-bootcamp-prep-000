c6baeda86f6f330a43b9f98e22c300279aa860a3
Sherry <sherryptk@gmail.com>
2017-09-19 13:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 //var recipes = {kale: "1 cup", sausage: "1 lb", potatoes: "1 lb", coconut milk: "2 cups"}
 
 var recipes={}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object.key = 'value';
+  return object
+}
+
+//returns an object with the orignal key value pairs and the new key value pair:
