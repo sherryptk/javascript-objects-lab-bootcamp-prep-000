1090eb4887cb06e3ffb1b3da7b3b372e4231d848
Sherry <sherryptk@gmail.com>
2017-09-19 15:29:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,5 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key]=value
-
-  return object
+  return object[key]=value
 }
